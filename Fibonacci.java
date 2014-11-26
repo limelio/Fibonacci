7a57689086f3025fe4a5a60fe827ef5133b8ab66
Emil <eharvey8399@hwdsb.on.ca>
2014-11-26 10:36:14 -0500

    Finish code; TODO: comments

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,20 +4,35 @@ import java.util.*;
 
 public class Fibonacci {
     public static void main(String[] args) {
-    	int userChoice = 0;
-    	int[] fibonacciArray;
-    	
-    	Scanner keyboard = new Scanner(System.in);
-    	
-    	System.out.print("Enter  number of Fibonacci numbers to generate [> 0]: ");
-    	
-    	while(userChoice == 0) {
-	    	try {
-	    		userChoice = keyboard.nextInt();
-	    	} catch(Exception e) {
-	    		System.out.print("Enter a value > 0: ");
-	    		keyboard.nextLine();
-	    	}
-    	}
+        int userChoice = 0;
+        int[] fibonacciArray;
+
+        Scanner keyboard = new Scanner(System.in);
+        while (userChoice < 2 || userChoice > 25) {
+            System.out.print("Enter  number of Fibonacci numbers to generate [2-25]: ");
+            try {
+                userChoice = keyboard.nextInt();
+            } catch (Exception e) {
+                keyboard.nextLine();
+            }
+        }
+
+        fibonacciArray = new int[userChoice];
+        fibonacciArray[0] = 1;
+        fibonacciArray[1] = 1;
+
+        System.out.println("Generating numbers...");
+        for (int fibSet = 2; fibSet < fibonacciArray.length; fibSet++) {
+            int newNum = (fibonacciArray[fibSet - 2] + fibonacciArray[fibSet - 1]);
+            fibonacciArray[fibSet] = newNum;
+        }
+        System.out.println("Done!");
+        System.out.println();
+        System.out.println();
+        System.out.println("Numbers generated:");
+        
+        for (int printFib : fibonacciArray) {
+            System.out.print("[" + printFib + "]");
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Fibonacci
 =========
+The Fibonacci Sequence is a set of numbers formed as follows: the first and
+second terms are both 1 and each succeeding term is formed by adding the two
+preceding terms. So the sequence starts 1, 1, 2, 3, 5, 8, …
+Write a program that displays the first 20 Fibonacci numbers. Your program will
+calculate the sequence in a method and return the result in an array.
+Enhance your program by prompting the user for the number
+of terms (you must decide on a reasonable maximum).
